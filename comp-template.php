1b7b7aa7bd08d29517f89e57dfd4d1f22bc4b81d
Jan Sládek <me@honzasladek.com>
2014-02-12 21:36:12 +0100

    Better styling for special prizes + more info (due to partners requests)

--- comp-template.php
+++ comp-template.php
@@ -10,6 +10,7 @@ Template Name: comp
     <div class='stars'></div>
     <p class='age'>Na nejlepší čeká řada cen a přidají se ke špičkám českého internetu. Soutěž je věkově omezena do 18 let (včetně).</p>
     <a href='http://souteze.juniorinternet.cz/register/' class='button'>Chci soutěžit</a>
+    <p class='comp-closing'>Přihlášky končí 17. 2. 2014</p>
     <div class='line'></div>
     <div class='orange'>Soutěžní disciplíny a ceny</div>
   </div>
@@ -43,8 +44,32 @@ Template Name: comp
         <div class='icon icon5'></div>
       </div>
     </div>
+    <div class="special-awards">
+      <h3 class="orange">Speciální ceny</h3>
+      <div class="special-row">
+        <div>
+          <h4>Nadace Vodafone</h4>
+          <p>Speciální cena pro obecně prospěšné projekty, kterou uděluje <a href="http://nadacevodafone.cz/">Nadace Vodafone</a>.</p>
+          <p class="special-note">V minulých letech ji vyhrál například projekt Drahomíra Hanáka <a href="http://www.schoolhelper.eu" target="_blank">SchoolHelper.eu</a> nebo Matěj Polák a projekt <a href="http://g-one.tv">studentské televize Gymnázia Dačice</a>.</p>
+        </div>
+        <div>
+          <h4>JuniorErb</h4>
+          <p>Speciální cena pro web prospěšný místu vašeho bydliště. V rámci této speciální ceny probíhají i krajská kola a vyhlašují se první tři místa. (<a href="http://www.juniorinternet.cz/wp-content/uploads/2014/02/2014_juniorerb_reditele.pdf">Informace pro učitele a ředitele v PDF</a>)</p>
+        </div>
+      </div>
+      <div class="special-row">
+        <div>
+          <h4>Cena veřejnosti</h4>
+          <p>Veřejnost uděluje cenu díky internetovému hlasování, které probíhá až do 10.&nbsp;3.</p>
+        </div>
+        <div>
+          <h4>Cena za uživatelský zážitek na mobilech</h4>
+          <p>Doba je mobilní a udělujeme proto speciální cenu projektu, který je nejlépe optimalizován pro mobilní zařízení.</p>
+        </div>
+      </div>
+    </div>
   </div>
-	
+
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 	<div class='inside'>
@@ -52,9 +77,13 @@ Template Name: comp
 	</div>
 
 	<?php endwhile; else: ?>
-		<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-	<?php endif; ?>
-	
+
+  <div class='inside'>
+	  <p><?php _e('Tato stránka zde bohužel není :('); ?></p>
+  </div>
+
+  <?php endif; ?>
+
 </div>
 
 <?php get_footer(); ?>
