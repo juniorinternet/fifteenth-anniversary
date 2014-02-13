d843493e36baaf49d4b5083a9ea19cba8ff5aabd
Tomas Privora <privorka95@gmail.com>
2014-02-13 23:26:38 +0100

    add partners to competition site

--- comp-template.php
+++ comp-template.php
@@ -5,7 +5,7 @@ Template Name: comp
 ?>
 <?php get_header(); ?>
 
-<div class='main'>
+<div class='main section'>
   <div class='inside'>
     <div class='stars'></div>
     <p class='age'>Na nejlepší čeká řada cen a přidají se ke špičkám českého internetu. Soutěž je věkově omezena do 18 let (včetně).</p>
@@ -44,12 +44,12 @@ Template Name: comp
         <div class='icon icon5'></div>
       </div>
     </div>
-    <div class="special-awards">
+    <div class="special-awards" id="special-awards">
       <h3 class="orange">Speciální ceny</h3>
       <div class="special-row">
         <div>
           <h4>Nadace Vodafone</h4>
-          <p>Speciální cena pro obecně prospěšné projekty, kterou uděluje <a href="http://nadacevodafone.cz/">Nadace Vodafone</a>.</p>
+          <p>Speciální cena pro web prospěšný místu vašeho bydliště. V rámci této speciální ceny probíhají i krajská kola a vyhlašují se první tři místa. (<a href="#junioerb-rules">Podrobná pravidla</a> <a href="http://www.juniorinternet.cz/wp-content/uploads/2014/02/2014_juniorerb_reditele.pdf">Informace pro učitele a ředitele v PDF</a>)</p>
           <p class="special-note">V minulých letech ji vyhrál například projekt Drahomíra Hanáka <a href="http://www.schoolhelper.eu" target="_blank">SchoolHelper.eu</a> nebo Matěj Polák a projekt <a href="http://g-one.tv">studentské televize Gymnázia Dačice</a>.</p>
         </div>
         <div>
@@ -70,20 +70,68 @@ Template Name: comp
     </div>
   </div>
 
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-	<div class='inside'>
-		<?php the_content(__('(celý článek...)')); ?>
-	</div>
-
-	<?php endwhile; else: ?>
+  <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
   <div class='inside'>
-	  <p><?php _e('Tato stránka zde bohužel není :('); ?></p>
+    <?php the_content(__('(celý článek...)')); ?>
+  </div>
+
+  <?php endwhile; else: ?>
+
+  <div class='inside'>
+    <p><?php _e('Tato stránka zde bohužel není :('); ?></p>
   </div>
 
   <?php endif; ?>
+</div>
 
+<div class='partners-section section'>
+  <div class='inside'>
+    <h2>Partneři</h2>
+    <span class='logo'>
+      <div class='first-part' data-icon='i'></div>
+      <div class='second-part' data-icon='i'></div>
+    </span>
+    <div class='partner-companies'>
+      <div class='comp'>
+        <h4>Generální partneři</h4>
+        <a href='http://nadacevodafone.cz/' class='partner vodafone'></a>
+        <a href='http://nic.cz/' class='partner cznic'></a>
+      </div>
+      <div class='comp'>
+        <h4>Partneři</h4>
+        <a href='http://www.czechict.cz/' class='partner ict'></a>
+        <a href='http://www.active24.cz/' class='partner active24'></a>
+        <a href='http://abdoc.net/' class='partner abdoc'></a>
+      </div>
+      <div class='comp'>
+        <h4>Partneři soutěží</h4>
+        <a href='http://prusaresearch.com/' class='partner prusa'></a>
+        <a href='http://www.rozectise.cz/' class='partner rozectise'></a>
+        <a href='http://www.dobryweb.cz/' class='partner dobryweb'></a>
+      </div>
+      <div class='comp'>
+        <h4>Mediální partneři</h4>
+        <a href='http://respekt.ihned.cz/' class='partner respekt'></a>
+        <a href='http://www.tyinternety.cz/' class='partner tyinternety'></a>
+        <a href='http://www.juniorinternet.cz/www.dvs.cz' class='partner dvs'></a>
+        <a href='http://www.juniorinternet.cz/www.lupa.cz' class='partner lupa'></a>
+        <a href='http://www.juniorinternet.cz/www.root.cz' class='partner root'></a>
+      </div>
+      <div class='comp'>
+        <h4>Podporovatelé</h4>
+        <a href='http://www.peoplecomm.cz/' class='partner peoplecomm'></a>
+        <a href='http://www.experienceu.com/cs/' class='partner experienceu'></a>
+      </div>
+      <div class='comp'>
+        <h4>Spolupracujeme</h4>
+        <a href='http://www.jsi.cz/' class='partner jsi'></a>
+        <a href='http://www.ceskyzavinac.cz/' class='partner zavinac'></a>
+        <a href='http://www.plzensky-kraj.cz/' class='partner pl-kraj'></a>
+        <a href='http://www.praha.eu/' class='partner praha'></a>
+      </div>
+    </div>
+  </div>
 </div>
 
 <?php get_footer(); ?>
