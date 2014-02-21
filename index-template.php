51f312486a7b22fa51c087c0c0a4fda1ef4b40af
Tomas Privora <privorka95@gmail.com>
2014-02-21 20:16:41 +0100

    move sponsors to separate file - easier edits

--- index-template.php
+++ index-template.php
@@ -79,50 +79,7 @@ Template Name: index
 
 <div class='main section'>
   <div class='inside'>
-    <h2>Partneři</h2>
-    <span class='logo'>
-      <div class='first-part' data-icon='i'></div>
-      <div class='second-part' data-icon='i'></div>
-    </span>
-    <div class='partner-companies'>
-      <div class='comp'>
-        <h4>Generální partneři</h4>
-        <a href='http://nadacevodafone.cz/' class='partner vodafone'></a>
-        <a href='http://nic.cz/' class='partner cznic'></a>
-      </div>
-      <div class='comp'>
-        <h4>Partneři</h4>
-        <a href='http://www.czechict.cz/' class='partner ict'></a>
-        <a href='http://www.active24.cz/' class='partner active24'></a>
-        <a href='http://abdoc.net/' class='partner abdoc'></a>
-      </div>
-      <div class='comp'>
-        <h4>Partneři soutěží</h4>
-        <a href='http://prusaresearch.com/' class='partner prusa'></a>
-        <a href='http://www.rozectise.cz/' class='partner rozectise'></a>
-        <a href='http://www.dobryweb.cz/' class='partner dobryweb'></a>
-      </div>
-      <div class='comp'>
-        <h4>Mediální partneři</h4>
-        <a href='http://respekt.ihned.cz/' class='partner respekt'></a>
-        <a href='http://www.tyinternety.cz/' class='partner tyinternety'></a>
-        <a href='http://www.juniorinternet.cz/www.dvs.cz' class='partner dvs'></a>
-        <a href='http://www.juniorinternet.cz/www.lupa.cz' class='partner lupa'></a>
-        <a href='http://www.juniorinternet.cz/www.root.cz' class='partner root'></a>
-      </div>
-      <div class='comp'>
-        <h4>Podporovatelé</h4>
-        <a href='http://www.peoplecomm.cz/' class='partner peoplecomm'></a>
-        <a href='http://www.experienceu.com/cs/' class='partner experienceu'></a>
-      </div>
-      <div class='comp'>
-        <h4>Spolupracujeme</h4>
-        <a href='http://www.jsi.cz/' class='partner jsi'></a>
-        <a href='http://www.ceskyzavinac.cz/' class='partner zavinac'></a>
-        <a href='http://www.plzensky-kraj.cz/' class='partner pl-kraj'></a>
-        <a href='http://www.praha.eu/' class='partner praha'></a>
-      </div>
-    </div>
+    <?php include 'sponsors.php'; ?>
   </div>
 </div>
 
